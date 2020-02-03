719fe363537f30404eb09ba5678e6243cc9da7d8
wei_lin <wei1234c@gmail.com>
2020-02-03 16:12:15 +0800

    add dbs.numbers(cls, n = 2, numbers = (0, 1))

--- codes/de_Bruijn_sequence.py
+++ codes/de_Bruijn_sequence.py
@@ -1,3 +1,5 @@
+# https://en.wikipedia.org/wiki/De_Bruijn_sequence
+
 class dbs:
     # https://zh.wikipedia.org/wiki/ASCII
     PUNCTUATIONS_1 = ''.join(map(chr, range(32, 48)))
@@ -11,22 +13,9 @@ class dbs:
 
 
     @classmethod
-    def _gen_sequence(cls, k, n):
-        """
-        de Bruijn sequence for alphabet k
-        and subsequences of length n.
-        """
+    def _gen_sequence(cls, collection, n):
 
-        try:
-            # let's see if k can be cast to an integer;
-            # if so, make our alphabet a list
-            _ = int(k)
-            alphabet = list(map(str, range(k)))
-
-        except (ValueError, TypeError):
-            alphabet = k
-            k = len(k)
-
+        k = len(collection)
         a = [0] * k * n
         sequence = []
 
@@ -46,19 +35,24 @@ class dbs:
 
         db(1, 1)
 
-        return "".join(alphabet[i] for i in sequence)
+        return [collection[i] for i in sequence]
 
 
     @classmethod
     def alphabets(cls, n = 2, alphabets = ALPHABETS):
-        return cls._gen_sequence(alphabets, n)
+        return "".join(cls._gen_sequence(alphabets, n))
 
 
     @classmethod
     def digits(cls, n = 2, digits = DIGITS):
-        return cls._gen_sequence(digits, n)
+        return "".join(cls._gen_sequence(digits, n))
 
 
     @classmethod
     def binary(cls, n = 2):
-        return cls._gen_sequence('01', n)
+        return "".join(cls._gen_sequence('01', n))
+
+
+    @classmethod
+    def numbers(cls, n = 2, numbers = (0, 1)):
+        return cls._gen_sequence(numbers, n)
---
+++ codes/test.py
@@ -0,0 +1,14 @@
+from de_Bruijn_sequence import dbs
+
+
+print(dbs.alphabets(alphabets = 'ABC'))
+
+print(dbs.alphabets())
+
+print(dbs.digits())
+
+print(dbs.binary())
+
+print(dbs.numbers())
+
+print(dbs.numbers(n = 2, numbers = range(9, 12)))
