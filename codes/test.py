a087869837dc716a0105bfdb50d06b5375ea1365
wei_lin <wei1234c@gmail.com>
2022-02-15 01:30:11 +0800

    add planning documents.

--- codes/test.py
+++ codes/test.py
@@ -1,14 +1,19 @@
-from de_Bruijn_sequence import dbs
+from de_Bruijn_sequence import dbs, combination_with_replacement
 
 
 print(dbs.alphabets(alphabets = 'ABC'))
 
-print(dbs.alphabets())
+print(dbs.alphabets(n = 2, alphabets = dbs.ALPHABETS))
 
-print(dbs.digits())
+print(dbs.digits(n = 2, digits = dbs.DIGITS))
 
-print(dbs.binary())
+print(dbs.binary(n = 2))
+print(combination_with_replacement.binary(n = 2))
 
-print(dbs.numbers())
+print(dbs.numbers(n = 2, numbers = (0, 1)))
 
 print(dbs.numbers(n = 2, numbers = range(9, 12)))
+
+keys = dbs.numbers(n = 3, numbers = range(256))
+# print(keys)
+print(len(keys))
